cfe97ff82d3c39f94892bf7bab186fb0eefd1e3f
customroaming <customroaming@gmail.com>
2026-06-11 14:54:53 +0100

    worked on basic invoice styler

--- invoice-list/src/App.tsx
+++ invoice-list/src/App.tsx
@@ -1,122 +1,97 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+interface Invoice {
+  id: number;
+  client: string;
+  status: "paid" | "unpaid" | "overdue";
+  email: string;
+  amount: number;
+}
+
+const invoices: Invoice[] = [
+  {
+    id: 1,
+    client: "Will",
+    status: "paid",
+    email: "wharper200@gmail.com",
+    amount: 32.3,
+  },
+  {
+    id: 2,
+    client: "Eseld",
+    status: "unpaid",
+    email: "eseldgmail.com",
+    amount: 333,
+  },
+  {
+    id: 3,
+    client: "Tom",
+    status: "overdue",
+    email: "bigdog@gmail.com",
+    amount: 69,
+  },
+  {
+    id: 4,
+    client: "Dawn",
+    status: "paid",
+    email: "dawny@gmail.com",
+    amount: 63.2,
+  },
+];
+
+export default function App() {
+  const [status, setStatus] = useState("all");
+  const filteredInvoices =
+    status == "all"
+      ? invoices
+      : invoices.filter((invoice) => invoice.status == status);
+
+  function getInvoices() {
+    return filteredInvoices.map((invoice) => {
+      return (
+        <div key={invoice.id} className="Invoice">
+          <p>ID: {invoice.id}</p>
+          <p>Name: {invoice.client}</p>
+          <p>Email: {invoice.email}</p>
+          <p>Amount: {invoice.amount}</p>
+          <p>Status: {invoice.status}</p>
+        </div>
+      );
+    });
+  }
+  function getAmountTotal() {
+    let sum = 0;
+    for (let i = 0; i < filteredInvoices.length; i++) {
+      sum = sum + filteredInvoices[i].amount;
+    }
+    return sum;
+  }
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
+      <div className="button-row">
+        <button className="filter-button" onClick={() => setStatus("all")}>
+          All
+        </button>
+        <button className="filter-button" onClick={() => setStatus("paid")}>
+          Paid
+        </button>
+        <button className="filter-button" onClick={() => setStatus("unpaid")}>
+          Unpaid
+        </button>
+        <button className="filter-button" onClick={() => setStatus("overdue")}>
+          Overdue
+        </button>
+      </div>
+      {getInvoices()}
+      {filteredInvoices.map((invoice) => (
+        <div key={invoice.id} className="Invoice">
           <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
+            {invoice.client} - £{invoice.amount}
           </p>
         </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      ))}
+      <p>Total invoice sum is: £{getAmountTotal()}</p>
     </>
-  )
+  );
 }
-
-export default App
