0a7215216ceb9a16f37a54e0b0e539f2795b8fee
Custom Roaming <customroaming@gmail.com>
2026-08-04 15:06:37 +0100

    nearly finished pdf creation

--- freelance-dashboard/components/invoices/pdfs/InvoicePDF.tsx
+++ freelance-dashboard/components/invoices/pdfs/InvoicePDF.tsx
@@ -31,6 +31,16 @@ Font.register({
       ),
       fontWeight: "bold",
     },
+    {
+      src: path.join(
+        process.cwd(),
+        "public",
+        "fonts",
+        "PlayfairDisplay-Italic.ttf",
+      ),
+      fontWeight: "normal",
+      fontStyle: "italic",
+    },
   ],
 });
 
@@ -50,10 +60,10 @@ Font.register({
 
 const styles = StyleSheet.create({
   page: {
-    padding: 40,
+    padding: 24,
     fontFamily: "Manrope",
     flexDirection: "column",
-    gap: 16,
+    gap: 32,
     letterSpacing: -0.5,
   },
   bold: {
@@ -62,12 +72,22 @@ const styles = StyleSheet.create({
   column: {
     flexDirection: "column",
   },
+  divider: {
+    borderBottomWidth: 1,
+    borderColor: "black",
+  },
+  gapLg: {
+    gap: 32,
+  },
   gapMd: {
     gap: 16,
   },
+  gapSm: {
+    gap: 8,
+  },
 
   heading: {
-    fontSize: 36,
+    fontSize: 32,
     fontWeight: "bold",
   },
   row: {
@@ -81,12 +101,20 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
   subheading: {
-    fontSize: 20,
+    fontSize: 18,
     fontWeight: "bold",
   },
+  solira: {
+    fontFamily: "PlayfairDisplay",
+    fontSize: 20,
+    fontStyle: "italic",
+  },
   p: {
-    fontSize: 16,
+    fontSize: 14,
     letterSpacing: 0,
+  },
+  textRight: {
+    textAlign: "right",
   },
 });
 
@@ -104,11 +132,9 @@ export default function InvoicePDF({
   return (
     <Document>
       <Page style={styles.page}>
-        <View>
+        <View style={[styles.column, styles.gapSm]}>
           <Text style={styles.heading}>INVOICE #{invoice.id}</Text>
-        </View>
-        <View>
-          <Text style={styles.strapline}>SOLIRA</Text>
+          <Text style={styles.solira}>Solira</Text>
         </View>
         <View style={[styles.row, styles.justifyBetween]}>
           <View style={[styles.column, styles.gapMd]}>
@@ -119,7 +145,59 @@ export default function InvoicePDF({
             </View>
           </View>
           <View>
-            <Text style={styles.subheading}>FROM</Text>
+            <View style={[styles.column, styles.gapMd]}>
+              <Text style={styles.subheading}>FROM</Text>
+              <View style={[styles.column, styles.p]}>
+                <View style={[styles.row, styles.gapSm]}>
+                  <Text style={styles.bold}>Invoice Date:</Text>
+                  <Text>{invoice.createdAt?.toLocaleDateString("en-GB")}</Text>
+                </View>
+                <View style={[styles.row, styles.gapSm]}>
+                  <Text style={styles.bold}>Email:</Text>
+                  <Text>wharper2000@gmail.com</Text>
+                </View>
+                <View style={[styles.row, styles.gapSm]}>
+                  <Text style={styles.bold}>Phone:</Text>
+                  <Text>07944233514</Text>
+                </View>
+              </View>
+            </View>
+          </View>
+        </View>
+        <View style={styles.divider}></View>
+        <View style={[styles.column, styles.gapMd]}>
+          <View style={[styles.row, styles.justifyBetween]}>
+            <View style={[styles.column, styles.gapMd]}>
+              <Text style={styles.subheading}>DESCRIPTION</Text>
+            </View>
+            <View>
+              <View style={[styles.column, styles.gapMd]}>
+                <Text style={styles.subheading}>AMOUNT</Text>
+              </View>
+            </View>
+          </View>
+          <View style={[styles.column, styles.gapSm, styles.p]}>
+            {invoiceItems.map((item) => (
+              <View key={item.id} style={[styles.row, styles.justifyBetween]}>
+                <View style={[styles.column, styles.gapSm]}>
+                  <Text>{item.description}</Text>
+                </View>
+                <Text style={styles.bold}>£{item.amount}</Text>
+              </View>
+            ))}
+          </View>
+        </View>
+        <View style={styles.divider}></View>
+        <View style={[styles.row, styles.justifyBetween]}>
+          <View style={[styles.column, styles.gapMd]}>
+            <Text style={styles.subheading}>TOTAL AMOUNT DUE</Text>
+          </View>
+          <View>
+            <View style={[styles.column, styles.gapMd]}>
+              <Text style={[styles.bold, styles.textRight]}>
+                £{invoiceItems.reduce((sum, item) => sum + item.amount, 0)}
+              </Text>
+            </View>
           </View>
         </View>
       </Page>
